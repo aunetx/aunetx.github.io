fb538e6a399ef8142a4a9d705936998d035ac913
aunetx <aunetx@yandex.com>
2021-09-16 21:46:56 +0200

    Update nav and footer, defaulted to md

---
+++ js/main.js
@@ -0,0 +1,33 @@
+"use strict";
+
+function check_current_page_id() {
+    let current_page = document.getElementById("current-page");
+    let link_list = document.getElementsByTagName('nav')[0].getElementsByTagName('li');
+    for (let link of link_list) {
+        let a = link.getElementsByTagName('a')[0]
+        if (a != current_page) {
+            a.onmouseover = () => {
+                current_page.className = "hide-current-page";
+            };
+            a.onmouseleave = () => {
+                current_page.className = "";
+            };
+        }
+    }
+}
+
+function check_full_height_taken() {
+    let footer = document.getElementsByTagName("footer")[0];
+    window.onresize = () => {
+        if (document.body.offsetHeight >= window.innerHeight) {
+            footer.className = "n_f full-height-taken"
+        } else {
+            footer.className = "n_f"
+        }
+    }
+}
+
+window.onload = function () {
+    check_current_page_id();
+    check_full_height_taken();
+}
